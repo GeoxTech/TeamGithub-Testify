836702ee36c893afca348cf87fbd640175a701fe
*GeoxTech* <*gibbs499@gmail.com*>
2022-06-10 16:43:24 +0100

    Automation Login and Sign-up testcase

--- untitled/src/test/java/SignUp.java
+++ untitled/src/test/java/SignUp.java
@@ -3,8 +3,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-public class LoginTest {
-
+public class SignUp {
 
     @Test
     public void start() throws InterruptedException {
@@ -12,7 +11,7 @@ public class LoginTest {
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
 
         //1. Open Chrome browser
-        //Import Selenuimdriver
+        //Import Selenuim Webdriver
         WebDriver driver = new ChromeDriver();
 
         //2. Input your selenium Demo Page URL (https://selenium-blog.herokuapp.com/)
@@ -22,25 +21,35 @@ public class LoginTest {
         //3. Maximize the Chrome browser
         driver.manage().window().maximize();
 
-        //4. Click the login button to sign-in
-        Thread.sleep(5000);
-        driver.findElement(By.xpath("/html/body/div[2]/div/a[1]")).click();
+        //4. Click the Sign-up button to sign-up
+        driver.findElement(By.xpath("/html/body/div[2]/div/a[2]")).click();
 
         //5. Wait global for page to fully loading
+        Thread.sleep(5000);
+
 
         //6. Register New User Email and Password
-        Thread.sleep(5000);
-        driver.findElement(By.id("session_email")).sendKeys("george43@mailinator.com");
-        driver.findElement(By.id("session_password")).sendKeys("password@123");
+
+        driver.findElement(By.id("user_username")).sendKeys("george44");
+        driver.findElement(By.id("user_email")).sendKeys("george44@mailinator.com");
+        driver.findElement(By.id("user_password")).sendKeys("password@123");
 
         Thread.sleep(5000);
         //7. Click on the Login button to Sign-In
-        driver.findElement(By.xpath("/html/body/div[2]/form/div[3]/div/input")).click();
+        driver.findElement(By.id("submit")).click();
 
         Thread.sleep(5000);
-        //9. Get text "Welcome to George43's Page
+        //8. Click on User1 article after signed in
+        driver.findElement(By.xpath("/html/body/div[2]/div[1]/ul/div/div/li[1]/a")).click();
+
+        //9. Get text "Welcome to User1's Page
         driver.findElement(By.xpath("/html/body/div[2]/h1")).getText();
 
+        Thread.sleep(5000);
+        //10. Kindly locate and click on the Learn XPath link
+        driver.findElement(By.xpath("/html/body/div[2]/div[1]/div/div/div[1]/a")).click();
+
+        Thread.sleep(5000);
         //11. Click on the Logout button
         driver.findElement(By.xpath("//*[@id=\"bs-example-navbar-collapse-1\"]/ul/li[3]/a")).click();
 
@@ -50,8 +59,6 @@ public class LoginTest {
 
         //Quit the browser
         driver.quit();
-
-
     }
 
 }
